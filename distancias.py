c80188126a90002d114770b86113329834879fde
Alexey <54072217+losk4@users.noreply.github.com>
2022-10-07 00:46:27 +0200

    levenshtein_edicion
    Primera versión de la recuperación del camino. Testeada pero susceptible a futuros cambios.

--- distancias.py
+++ distancias.py
@@ -1,5 +1,21 @@
+""" 
+PROYECTO COORDINADO SAR-ALT (DISTANCIAS)
+Autores:    Villanueva Tarazona, Jose
+            Mengliev, Alexey
+            Apellido/s, Nombre
+            Apellido/s, Nombre
+Grupo:      L1-4CO11             
+"""
+
+from re import S
 import numpy as np
 
+# [ALEXEY]
+# Debajo utilizo esta misma versión que nos dan pero adaptada a mi gusto (porque soy tonto y me lío con la matriz).
+# He hecho que cuadre con el grafo de las transparencias (o eso creo), intercambiando ejes.
+# Elegid solo UNA versión para realizar los demás algoritmos y me lo comentáis para que cambie la edición más adelante.
+
+# Versión base.
 def levenshtein_matriz(x, y, threshold=None):
     # esta versión no utiliza threshold, se pone porque se puede
     # invocar con él, en cuyo caso se ignora
@@ -17,9 +33,88 @@ def levenshtein_matriz(x, y, threshold=None):
             )
     return D[lenX, lenY]
 
+# [ALEXEY]
+# Versión modificada. SIN OPTIMIZAR, la he hecho del tirón.
+# Recuperación de operaciones de edición. 
 def levenshtein_edicion(x, y, threshold=None):
-    # a partir de la versión levenshtein_matriz
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenY + 1, lenX + 1), dtype=np.int)
+
+    for i in range(1, lenY + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenX + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenY + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[j - 1] != y[i - 1]),
+            )
+
+    # Diapo. 25: ... Importante: tras finalizar el cálculo de la distancia.
+    # Esto significa que hay que reconstruir, no acumular. Que pereza.
+
+    # Lista de tuplas de edición.
+    B = []
+
+    '''
+    EJEMPLO
+    
+    x = ejemplo (eje J)
+    y = campos  (eje I)
+
+            s   6   6   6   6   6   5   5   5                          
+            o   5   5   5   5   5   4   4   4
+    I       p   4   4   4   4   4   3   4   5
+            m   3   3   3   3   3   4   5   6
+            a   2   2   2   3   4   5   6   7
+            c   1   1   2   3   4   5   6   7
+                0   1   2   3   4   5   6   7
+                    e   j   e   m   p   l   o
+
+                                J
+
+    '''
+    
+    # Empezamos desde la "esquina" superior derecha de la matriz de Levenshtein.
+    i = lenY
+    j = lenX
+
+    while True:
+        # Final del camino, estamos en la "esquina" inferior izquierda.
+        if i == 0 and j == 0:
+            break
+        # Estamos en la "pared" inferior, hay que eliminar.
+        elif i == 0:
+            B.append((x[j - 1], ''))
+            j = j - 1
+        # Estamos en la "pared" izquierda, hay que insertar.
+        elif j == 0:
+            B.append(('', y[i - 1]))
+            i = i - 1
+
+        # Elegimos la operación de edicion óptima. Si hay mas de una, la que sea.
+        # En este caso, se prioriza SUB (tiene que ir primero) > BOR > INS en caso de empate.
+        else:
+            op = [D[i - 1][j - 1], D[i][j - 1], D[i - 1][j]]
+            
+            minval = min(op)
+            minindex = op.index(minval)
+
+            if minindex == 0:
+                B.append((x[j - 1], y[i - 1]))
+                i = i - 1
+                j = j - 1
+            elif minindex == 1:
+                B.append((x[j - 1], ''))
+                j = j - 1
+            else:
+                B.append(('', y[i - 1]))
+                i = i - 1
+    
+    # Revertimos la secuencia y retornamos la tupla (distancia, secuencia)
+    B.reverse()
+    return D[lenY, lenX], B
 
 def levenshtein_reduccion(x, y, threshold=None):
     # completar versión con reducción coste espacial
@@ -45,7 +140,7 @@ def damerau_restricted_edicion(x, y, threshold=None):
 
 def damerau_restricted(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
-     return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
 
 def damerau_intermediate_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein intermedia con matriz
@@ -61,7 +156,7 @@ def damerau_intermediate(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
     return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
 
-opcionesSpell = {
+opcionesSpellBase = {
     'levenshtein_m': levenshtein_matriz,
     'levenshtein_r': levenshtein_reduccion,
     'levenshtein':   levenshtein,
@@ -72,9 +167,18 @@ opcionesSpell = {
     'damerau_i':     damerau_intermediate
 }
 
-opcionesEdicion = {
+opcionesEdicionBase = {
     'levenshtein': levenshtein_edicion,
     'damerau_r':   damerau_restricted_edicion,
     'damerau_i':   damerau_intermediate_edicion
 }
 
+# Para no pillar TODOS los tests de momento, id modificando esto mejor.
+opcionesSpell = {
+    'levenshtein_m': levenshtein_matriz,
+}
+
+opcionesEdicion = {
+    'levenshtein': levenshtein_edicion
+}
+#
