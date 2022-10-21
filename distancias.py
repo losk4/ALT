2dcf492d5841ddaa92482636c835594f47186917
diegof12 <115708790+diegof12@users.noreply.github.com>
2022-10-21 14:51:06 +0200

    Add files via upload

--- distancias.py
+++ distancias.py
@@ -1,10 +1,10 @@
-""" 
+"""
 PROYECTO COORDINADO SAR-ALT (DISTANCIAS)
 Autores:    Villanueva Tarazona, Jose
             Mengliev, Alexey
-            Apellido/s, Nombre
-            Apellido/s, Nombre
-Grupo:      L1-4CO11             
+            Fernández Sutil, Diego
+            Marcos Bravo, Pablo
+Grupo:      L1-4CO11
 """
 
 from re import S
@@ -35,7 +35,7 @@ def levenshtein_matriz(x, y, threshold=None):
 
 # [ALEXEY]
 # Versión modificada. SIN OPTIMIZAR, la he hecho del tirón.
-# Recuperación de operaciones de edición. 
+# Recuperación de operaciones de edición.
 def levenshtein_edicion(x, y, threshold=None):
     lenX, lenY = len(x), len(y)
     D = np.zeros((lenY + 1, lenX + 1), dtype=np.int)
@@ -59,11 +59,11 @@ def levenshtein_edicion(x, y, threshold=None):
 
     '''
     EJEMPLO
-    
+
     x = ejemplo (eje J)
     y = campos  (eje I)
 
-            s   6   6   6   6   6   5   5   5                          
+            s   6   6   6   6   6   5   5   5
             o   5   5   5   5   5   4   4   4
     I       p   4   4   4   4   4   3   4   5
             m   3   3   3   3   3   4   5   6
@@ -75,7 +75,7 @@ def levenshtein_edicion(x, y, threshold=None):
                                 J
 
     '''
-    
+
     # Empezamos desde la "esquina" superior derecha de la matriz de Levenshtein.
     i = lenY
     j = lenX
@@ -98,7 +98,7 @@ def levenshtein_edicion(x, y, threshold=None):
         # Si quereis cambiar el orden, tendriais que checkear los SUBs de coste 0 antes de esto.
         else:
             op = [D[i - 1][j - 1], D[i][j - 1], D[i - 1][j]]
-            
+
             minval = min(op)
             minindex = op.index(minval)
 
@@ -112,7 +112,7 @@ def levenshtein_edicion(x, y, threshold=None):
             else:
                 B.append(('', y[i - 1]))
                 i = i - 1
-    
+
     # Revertimos la secuencia y retornamos la tupla (distancia, secuencia)
     B.reverse()
     return D[lenY, lenX], B
@@ -133,7 +133,7 @@ def levenshtein_reduccion(x, y, threshold=None):
                 row2[j] + 1,
                 row2[j - 1] + (x[j - 1] != y[i - 1])
             )
-    
+
     return row1[-1]
 
 # [ALEXEY]
@@ -154,7 +154,7 @@ def levenshtein(x, y, threshold):
         # Os hago la version pocha de parada por threshold, que tengo prisa. Usad esto si no teneis nada mejor.
         if all(d > threshold for d in row1):
             return threshold + 1
-    
+
     return min(row1[-1], threshold + 1)
 
 def levenshtein_cota_optimista(x, y, threshold):
@@ -163,28 +163,126 @@ def levenshtein_cota_optimista(x, y, threshold):
 def damerau_restricted_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein restringida con matriz
     lenX, lenY = len(x), len(y)
-    # COMPLETAR
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+                if i > 1:
+                    D[i - 2][j - 2] + (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]),
+            )
     return D[lenX, lenY]
 
 def damerau_restricted_edicion(x, y, threshold=None):
-    # partiendo de damerau_restricted_matriz añadir recuperar
-    # secuencia de operaciones de edición
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+                if i > 1:
+                    D[i - 2][j - 2] + (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]),
+            )
+
+    # Lista de tuplas de edición.
+    B = []
+
+    # Empezamos desde la "esquina" superior derecha de la matriz de Levenshtein.
+    i = lenY
+    j = lenX
+
+    while True:
+        # Final del camino, estamos en la "esquina" inferior izquierda.
+        if i == 0 and j == 0:
+            break
+        # Estamos en la "pared" inferior, hay que eliminar.
+        elif i == 0:
+            B.append((x[j - 1], ''))
+            j = j - 1
+        # Estamos en la "pared" izquierda, hay que insertar.
+        elif j == 0:
+            B.append(('', y[i - 1]))
+            i = i - 1
+
+        # Elegimos la operación de edicion óptima. Si hay mas de una, la que sea.
+        # En este caso, se prioriza SUB > BOR > INS en caso de empate.
+        # Si quereis cambiar el orden, tendriais que checkear los SUBs de coste 0 antes de esto.
+        else:
+            op = [D[i - 1][j - 1], D[i][j - 1], D[i - 1][j]]
+
+            minval = min(op)
+            minindex = op.index(minval)
+
+            if minindex == 0:
+                B.append((x[j - 1], y[i - 1]))
+                i = i - 1
+                j = j - 1
+            elif minindex == 1:
+                B.append((x[j - 1], ''))
+                j = j - 1
+            else:
+                B.append(('', y[i - 1]))
+                i = i - 1
+
+    # Revertimos la secuencia y retornamos la tupla (distancia, secuencia)
+    B.reverse()
+    return D[lenY, lenX], B
 
 def damerau_restricted(x, y, threshold=None):
-    # versión con reducción coste espacial y parada por threshold
-    return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    #NO COMPLETO
+    lenX, lenY = len(x) + 1, len(y) + 1
+    row1 = list(range(lenX))
+    row2 = [None] * lenX
+
+    for i in range(1, lenY):
+        row1, row2 = row2, row1
+        row1[0] = i
+        for j in range(1, lenX):
+            row1[j] = min(
+                row1[j - 1] + 1,
+                row2[j] + 1,
+                row2[j - 1] + (x[j - 1] != y[i - 1])
+                if j > 1:
+
+            )
+
+    return row1[-1]
 
 def damerau_intermediate_matriz(x, y, threshold=None):
-    # completar versión Damerau-Levenstein intermedia con matriz
+    #NO COMPLETO
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+                if i > 1:
+                    D[i - 2][j - 2] + (x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2]),
+            )
     return D[lenX, lenY]
+
 
 def damerau_intermediate_edicion(x, y, threshold=None):
     # partiendo de matrix_intermediate_damerau añadir recuperar
     # secuencia de operaciones de edición
     # completar versión Damerau-Levenstein intermedia con matriz
     return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
-    
+
 def damerau_intermediate(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
     return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
